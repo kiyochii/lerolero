153112e9b0c28bb90f9a6d1eb1be26b1f6dd6b6b
Dan <danshoji@usp.br>
2025-03-20 10:33:43 -0300

    implementa o esqueleto do programa

--- lerolero.py
+++ lerolero.py
@@ -2,3 +2,12 @@
 gera frases de efeito sem
 significado real***
 
+import random
+
+parte1 = []
+parte2 = []
+parte3 = []
+
+
+print(random.choice(parte1), random.choice(parte2), random.choice(parte3))
+
