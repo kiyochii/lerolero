2aef6d6aa034a942e013fb400a03a63206e574f0
Dan <danshoji@usp.br>
2025-03-20 10:45:38 -0300

    Esqueleto Ingles

--- lerolero.py
+++ lerolero.py
@@ -8,6 +8,12 @@ parte1 = []
 parte2 = []
 parte3 = []
 
+lingua = int(input("Escolha a lingua 1- Português 2- Ingles"))
+if lingua == 2
+	parte1 = []
+	parte2 = []
+	parte3 = []
+
 
 print(random.choice(parte1), random.choice(parte2), random.choice(parte3))
 
