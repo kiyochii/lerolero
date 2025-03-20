549ccaaa5c7a32e19d7edaf26e3df8b203066349
João Vitor <am3nic2006@usp.br>
2025-03-20 10:53:31 -0300

    inglês

--- lerolero.py
+++ lerolero.py
@@ -10,11 +10,21 @@ parte3 = []
 
 lingua = int(input("Escolha a lingua 1- Português 2- Ingles"))
 if lingua == 2
-	parte1 = []
-	parte2 = []
-	parte3 = []
+	parte1 = [
+	"O sistema em desenvolvimento",
+	"O novo protocolo de comunicação",
+	"O algoritmo otimizado"]
+	parte2 = [
+	"possui excelente desempenho",
+	"oferece garantias de precisão acima da média",
+	"preenche uma lacuna significativa"]
+	parte3 = [
+	"nas aplicações a que se destina",
+	"em relação às opções disponiveis no mercado",
+	"provendo ampla vantagem competitiva a seus usuários"]
 
 
 print(random.choice(parte1), random.choice(parte2), random.choice(parte3))
 
 #teste
+
